fb43891907897a9375709186cfbcfbc87fd47b9f
Tamara Berrocal <tamaraberrocal@gmail.com>
2019-08-07 14:59:25 -0300

    Modificando el comportamiento de las tareas

--- assets/javascript/todolist.js
+++ assets/javascript/todolist.js
@@ -1,8 +1,16 @@
 //$("li").on("click", function() { $(this).toggleClass("done") });
 $("ul").on("click", "li", function() { $(this).toggleClass("done") });
 
+$("ul").on("mouseover", "li", function() {
+    $(this).prepend(`<i class='fa fa-trash' aria-hidden='true'></i> `);
+});
+
+$("ul").on("mouseout", "li", function() {
+    $(this).next().remove();
+});
+
 //$("span").on("click", function(e) {
-$("ul").on("click", "span", function(e) {
+$("ul").on("click", ".borrar", function(e) {
     //$(this).parent().hide(); //Esconde el elemento
     $(this).parent().remove(); //Remueve el elemento
     e.stopPropagation();
@@ -10,7 +18,7 @@ $("ul").on("click", "span", function(e) {
 
 $("input[type='text']").on("keypress", (function(e) { //El [type='text'] en el selector permite limitar la selección a los input de tipo "text"
     if(e.key === "Enter") {
-        $("ul").append(`<li><span>[borrar] <i class='fa fa-trash' aria-hidden='true'></i></span> ${$(this).val()}</li>`);
+        $("ul").append(`<li><i class="far fa-square"></i> ${$(this).val()}</li>`);
         $(this).val(""); //$(this).val() recupera el valor del input
         event.stopPropagation();
     }
